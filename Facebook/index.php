9d8cf029601a8806c6f7c74fb0fca90a03d6c97b
ArturoAG <noe_170695@hotmail.com>
2018-01-31 22:39:28 -0600

    cambios generales

--- Facebook/index.php
+++ Facebook/index.php
@@ -3,53 +3,53 @@
 	echo $user_agent;
 	function getPlatform($user_agent){
 		if(strpos($user_agent, 'Windows NT 10.0') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 6.3') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 6.2') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 6.1') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 6.0') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 5.1') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 5.2') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows NT 5.0') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows ME') !== FALSE)
-			header("Location: Facebook/index.html");
+			header("Location: Facebook/Facebook.htm");
 		elseif(strpos($user_agent, 'Win98') !== FALSE)
-			header("Location: Facebook/index.html");
+			header("Location: Facebook/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows Phone') !== FALSE)
-			header("Location: Mobile/index.html");
+			header("Location: Mobile/Facebook.htm");
 		elseif(strpos($user_agent, 'Windows') !== FALSE)
-			header("Location: Facebook/index.html");
+			header("Location: Facebook/Facebook.htm");
 		elseif(strpos($user_agent, 'iPhone') !== FALSE)
-			header("Location: OS/index.html");
+			header("Location: OS/Facebook.htm");
 		elseif(strpos($user_agent, 'iPad') !== FALSE)
-			header("Location: OS/index.html");
+			header("Location: OS/Facebook.htm");
 		elseif(strpos($user_agent, 'Debian') !== FALSE)
-			header("Location: Web/index.html");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Ubuntu') !== FALSE)
-			header("Location: Web/index.html");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Elementary OS') !== FALSE)
-			header("Location: Web/index.html");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Kubuntu') !== FALSE)
-			header("Location: Web/index.html");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Linux') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Android/Facebook.htm");
 		elseif(strpos($user_agent, 'BlackBerry') !== FALSE)
-			header("Location: Mobile/index.html");
-		elseif(strpos($user_agent, 'Android') !== FALSE)
-			header("Location: Android/index.htm");
+			header("Location: Mobile/Facebook.htm");
+		elseif(strpos($user_agent, 'Android') || strpos($user_agent, 'Linux') !== FALSE)
+			header("Location: Android/Facebook.htm");
 		elseif(strpos($user_agent, 'Mobile') !== FALSE)
-			header("Location: Mobile/index.html");
+			header("Location: Mobile/Facebook.htm");
 		elseif(strpos($user_agent, 'Mac OS X+') || strpos($user_agent, 'CFNetwork+') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		elseif(strpos($user_agent, 'Macintosh') !== FALSE)
-			header("Location: Web/index.htm");
+			header("Location: Web/Facebook.htm");
 		else
 			header("Location: Facebook/Facebook.htm");
 	}
